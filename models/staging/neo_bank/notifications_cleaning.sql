a5655fa2dc0ddb9e2f7ec3b815e1703a76c5844d
Nath Bel <hachty@hotmail.com>
2024-03-19 14:49:46 +0000

    suppression du fichier de cleaning notifications en doublon

--- models/staging/neo_bank/notifications_cleaning.sql
+++
@@ -1,7 +0,0 @@
-SELECT * ,
-FROM {{ ref('stg_neo_bank__notifications') }}
-WHERE reason IS NOT NULL
-  AND channel IS NOT NULL
-  AND status IS NOT NULL
-  AND user_id IS NOT NULL
-  AND created_date IS NOT NULL
