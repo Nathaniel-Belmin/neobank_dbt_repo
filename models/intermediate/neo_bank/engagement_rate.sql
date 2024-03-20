0ac0a265a8a8ee6b7213a1943828c63dc34f889b
Nathan Chatard <nathan.chatard1@gmail.com>
2024-03-20 11:47:19 +0000

    engagement rate per month

--- models/intermediate/neo_bank/engagement_rate.sql
+++ models/intermediate/neo_bank/engagement_rate.sql
@@ -11,24 +11,24 @@ WITH user_transactions AS (
 user_lifetime AS (
     SELECT
         user_id,
-        DATE_DIFF(MAX(max_created_date), MIN(min_created_date), DAY) AS lifetime_days
+        DATE_DIFF(MAX(max_created_date), MIN(min_created_date), MONTH) AS lifetime_months
     FROM user_transactions
     GROUP BY user_id
 ),
 
 baseline AS (
     SELECT
-        (SELECT APPROX_QUANTILES(SAFE_DIVIDE(num_transactions, lifetime_days), 100)[OFFSET(50)] FROM user_transactions, user_lifetime) AS median_transactions
+        (SELECT APPROX_QUANTILES(SAFE_DIVIDE(num_transactions, lifetime_months), 100)[OFFSET(50)] FROM user_transactions, user_lifetime) AS median_transactions
 ),
 
 engagement_status AS (
     SELECT
         ul.user_id,
-        ul.lifetime_days,
+        ul.lifetime_months,
         COALESCE(ut.num_transactions, 0) AS num_transactions,
-        ROUND(SAFE_DIVIDE(num_transactions, lifetime_days), 2) as engagement_rate,
+        ROUND(SAFE_DIVIDE(num_transactions, lifetime_months), 2) as engagement_rate,
         CASE
-            WHEN SAFE_DIVIDE(ut.num_transactions, ul.lifetime_days) > (SELECT median_transactions FROM baseline) THEN 'Engaged'
+            WHEN SAFE_DIVIDE(ut.num_transactions, ul.lifetime_months) > (SELECT median_transactions FROM baseline) THEN 'Engaged'
             ELSE 'Not Engaged'
         END AS engagement_status
     FROM user_lifetime ul
