c73f30f4462bf5a8a1b4723a009bd4007a5f5903
Célia Rohart <celia.rohart@gmail.com>
2024-03-21 09:51:13 +0000

    merge final

--- models/intermediate/neo_bank/notifications_reason_repartition.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-  reason,
-  COUNT(*) AS nb_notifications
-FROM {{ ref('stg_neo_bank__notifications') }}  
-GROUP BY reason
---
+++ models/mart/neobank/merge_users_final.sql
@@ -0,0 +1,30 @@
+select
+    cs.user_id,
+    cs.churner,
+    u.birth_year,
+    u.country,
+    u.city,
+    u.created_date,
+    u.settings_crypto_unlocked,
+    u.attributes_notifications_marketing_email,
+    u.attributes_notifications_marketing_push,
+    u.num_contacts,
+    uac.tranche_age,
+    er.plan,
+    er.lifetime_months,
+    er.num_transactions,
+    er.total_amount_usd, 
+    er.engagement_rate,
+    er.amount_usd_per_lifetime,
+    er.engagement_status
+from
+   {{ ref('users_cleaning') }} as u 
+inner join
+    {{ ref('churn_status') }} as cs
+using (user_id)
+inner join
+    {{ ref('user_id_age_category') }} as uac
+using (user_id)
+inner join
+    {{ ref('engagement_rate_by_month_and_plan') }} as er
+using (user_id)
