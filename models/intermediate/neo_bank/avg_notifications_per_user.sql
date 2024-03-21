df6076fc24b95a0099b9c8952a6c81a7e47786b4
Nath Bel <hachty@hotmail.com>
2024-03-21 09:26:12 +0000

    Cleaning DBT

--- models/intermediate/neo_bank/avg_notifications_per_user.sql
+++
@@ -1,6 +0,0 @@
-SELECT AVG(nb_notifications_per_user) AS avg_notifications_per_user
-FROM (
-    SELECT COUNT(*) AS nb_notifications_per_user
-    FROM {{ ref('stg_neo_bank__notifications') }} 
-    GROUP BY user_id
-) 
